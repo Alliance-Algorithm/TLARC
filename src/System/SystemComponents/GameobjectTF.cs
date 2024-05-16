1bd4f268bbc8331ff7d18fd01ee829625fde9e55
alray <1780284652@qq.com>
2024-05-16 18:11:53 +0000

    change tranform to pose2d

--- src/System/SystemComponents/GameobjectTF.cs
+++ src/System/SystemComponents/GameobjectTF.cs
@@ -35,14 +35,22 @@ namespace AllianceDM.StdComponent
             }
             if (Args[1] == "W" && Args.Length == 3)
             {
-                IOManager.RegistryMassage(Args[2], (Odometry msg) =>
+                // IOManager.RegistryMassage(Args[2], (Odometry msg) =>
+                // {
+                //     position = new Vector2(-(float)msg.Pose.Pose.Position.Y, (float)msg.Pose.Pose.Position.X);
+                //     // yaw (z-axis rotation)
+                //     var q = msg.Pose.Pose.Orientation;
+                //     double siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
+                //     double cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
+                //     angle = Math.Atan2(siny_cosp, cosy_cosp);
+                // }
+                // );
+
+                IOManager.RegistryMassage(Args[2], (Pose2D msg) =>
                 {
-                    position = new Vector2(-(float)msg.Pose.Pose.Position.Y, (float)msg.Pose.Pose.Position.X);
+                    position = new Vector2((float)msg.X, (float)msg.Y);
                     // yaw (z-axis rotation)
-                    var q = msg.Pose.Pose.Orientation;
-                    double siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
-                    double cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
-                    angle = Math.Atan2(siny_cosp, cosy_cosp);
+                    angle = msg.Theta;
                 }
                 );
             }
