feadd5ecad8fdd02bc1181c8c06fb189792f4877
alray <1780284652@qq.com>
2024-05-18 17:06:51 +0000

    gimbal forward

--- obj/Release/net8.0/linux-x64/decision_maker.AssemblyInfo.cs
+++ obj/Release/net8.0/linux-x64/decision_maker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("decision_maker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2811b900124d4d4aadac10e89584ff85dababaa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871203be04ec7233f64f87221a0c66fdf5fdf434")]
 [assembly: System.Reflection.AssemblyProductAttribute("decision_maker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("decision_maker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
