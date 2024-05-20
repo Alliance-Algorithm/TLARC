a7b55775e5ff653befe5601fd0eb8d36b5a8bbc2
alray <1780284652@qq.com>
2024-05-20 08:39:25 +0000

    v1.5 fixed trimble dwa (close it)

--- obj/Debug/net8.0/decision_maker.AssemblyInfo.cs
+++ obj/Debug/net8.0/decision_maker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("decision_maker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+066ce1c43470ea0204773db8a3f07d3aa9b8cdf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f56492b1945ede1a9ffebadbccb838775cea9ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("decision_maker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("decision_maker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
