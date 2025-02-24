477f86eeefd5651f5f6f5888e8a22645b05f1fec
AlrayQiu <1780284652@qq.com>
2025-02-24 12:24:31 +0800

    Test: Run plan

--- src/TrajectoryTracer/Tracer.cs
+++ src/TrajectoryTracer/Tracer.cs
@@ -7,6 +7,8 @@ namespace TrajectoryTracer;
 class Tracer : Component
 {
     IO.ROS2Msgs.Geometry.Pose2D pose2D;
+
+    double velocityRatio = 0.5;
     public Vector3d velocity;
     [ComponentReferenceFiled]
     IPositionVelocityController controller;
@@ -19,6 +21,7 @@ class Tracer : Component
     public override void Update()
     {
         velocity = controller.ControlVolume(sentry.Position);
+        velocity = velocityRatio * velocity;
         velocity = Quaterniond.AxisAngleR(Vector3d.AxisZ, -sentry.AngleR) * velocity;
         pose2D.Publish((new((float)velocity.x, (float)velocity.y), 0));
     }
--- src/TrajectoryTracer/VelocityController/ModelPredictController.cs
+++ src/TrajectoryTracer/VelocityController/ModelPredictController.cs
@@ -13,8 +13,10 @@ class VelocityMPC : Component, IPositionVelocityController
     public double rho = 10;
     public double[] U;
     public double[] uOut;
-    public const double uMax = 5;
+    public const double uMax = 1 * 0.1;
     public const double uMin = -uMax;
+    public const double vMax = 1;
+    public const double vMin = -vMax;
 
     LinearConstraintCollection constraintCollection;
     public Vector3d ControlVolume(Vector3d Position)
@@ -91,20 +93,51 @@ class VelocityMPC : Component, IPositionVelocityController
 
         constraintCollection = new();
 
-        for (int i = 0; i < car.ControlVolumeSize * P; i++)
+        var ConstantA1 = new double[car.ControlVolumeSize * P];
+        var ConstantA2 = new double[car.ControlVolumeSize * P];
+        for (int i = 0; i < car.ControlVolumeSize * P; i += 2)
         {
-            var ConstantA = new double[car.ControlVolumeSize * P];
-            ConstantA[i] = 1;
+            ConstantA1[i] = 1;
+            ConstantA1[i + 1] = 1;
             constraintCollection.Add(new(car.ControlVolumeSize * P)
             {
-                CombinedAs = ConstantA,
-                Value = uMin - U[i % 2],
+                CombinedAs = ConstantA1,
+                Value = vMin - U[0],
                 ShouldBe = ConstraintType.GreaterThanOrEqualTo
             });
             constraintCollection.Add(new(car.ControlVolumeSize * P)
             {
-                CombinedAs = ConstantA,
-                Value = uMax - U[i % 2],
+                CombinedAs = ConstantA1,
+                Value = vMax - U[0],
+                ShouldBe = ConstraintType.LesserThanOrEqualTo
+            });
+            constraintCollection.Add(new(car.ControlVolumeSize * P)
+            {
+                CombinedAs = ConstantA2,
+                Value = vMin - U[1],
+                ShouldBe = ConstraintType.GreaterThanOrEqualTo
+            });
+            constraintCollection.Add(new(car.ControlVolumeSize * P)
+            {
+                CombinedAs = ConstantA2,
+                Value = vMax - U[1],
+                ShouldBe = ConstraintType.LesserThanOrEqualTo
+            });
+        }
+        for (int i = 0; i < car.ControlVolumeSize * P; i++)
+        {
+            var ConstantB = new double[car.ControlVolumeSize * P];
+            ConstantB[i] = 1;
+            constraintCollection.Add(new(car.ControlVolumeSize * P)
+            {
+                CombinedAs = ConstantB,
+                Value = uMin,
+                ShouldBe = ConstraintType.GreaterThanOrEqualTo
+            });
+            constraintCollection.Add(new(car.ControlVolumeSize * P)
+            {
+                CombinedAs = ConstantB,
+                Value = uMax,
                 ShouldBe = ConstraintType.LesserThanOrEqualTo
             });
         }
