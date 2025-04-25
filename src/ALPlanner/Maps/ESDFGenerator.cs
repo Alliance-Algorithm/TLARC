cc42c4624b24e0a0b77140dc16137b896c0bec01
Alray <1780284652@qq.com>
2025-04-25 19:15:01 +0800

    Develop: add initializer for png

--- src/ALPlanner/Maps/ESDFGenerator.cs
+++ src/ALPlanner/Maps/ESDFGenerator.cs
@@ -33,8 +33,6 @@ public class ESDFGenerator : Component, IESDF
     private sbyte[,] _map;
     Queue<DynamicMapData> _dynamicMaps;
     private int[,,] _obstacles;
-    private sbyte[,] _colored;
-
     private IO.ROS2Msgs.Nav.OccupancyGrid _dynamicMapReceiver;
     private IO.ROS2Msgs.Nav.OccupancyGrid _esdfPublisher;
 
@@ -55,14 +53,12 @@ public class ESDFGenerator : Component, IESDF
     public override void Start()
     {
         if (mapPath == "")
-        {
             _staticMap = new() { _resolution = 0.2f, _size_x = 70, _size_y = 70, _staticObs = new int[70, 70, 0], _staticMap = new sbyte[70, 70] };
-        }
-        else
-        {
-            var json = File.ReadAllText(TlarcSystem.RootPath + mapPath);
-            _staticMap = JsonConvert.DeserializeObject<ESDFMapData>(json);
-        }
+        else if (mapPath.EndsWith(".json"))
+            ESDFBuilder.InitWithJson(ref _staticMap, mapPath);
+        else if (mapPath.EndsWith(".png"))
+            ESDFBuilder.InitWithImage(ref _staticMap, mapPath);
+
         _dynamicMapReceiver = new(IOManager);
         _dynamicMapReceiver.Subscript(dynamicMapTopicName, data =>
         {
@@ -96,78 +92,7 @@ public class ESDFGenerator : Component, IESDF
     public override void Update()
     {
         _sentry_position = sentry.Position;
-        Buffer.BlockCopy(_staticMap.Map, 0, _map, 0, _map.Length * sizeof(sbyte));
-        Buffer.BlockCopy(_staticMap.Obstacles, 0, _obstacles, 0, _obstacles.Length * sizeof(int));
-        _colored = new sbyte[SizeX, SizeY];
-
-        Queue<(int x, int y)> openList = new Queue<(int x, int y)>();
-        foreach (var _dynamicMap in _dynamicMaps)
-        {
-            for (int i = 0, k = _dynamicMap._map.GetLength(0); i < k; i++)
-                for (int j = 0; j < k; j++)
-                {
-                    var k_2 = k / 2;
-                    var x = (int)Math.Round((i) * _dynamicMap.forward.Cos - (j) * _dynamicMap.forward.Sin + _dynamicMap.offsetX);
-                    var y = (int)Math.Round((j) * _dynamicMap.forward.Cos + (i) * _dynamicMap.forward.Sin + _dynamicMap.offsetY);
-                    if (x < 0 || x >= SizeX)
-                        continue;
-                    if (y < 0 || y >= SizeY)
-                        continue;
-                    if (_dynamicMap._map[i, j] == -1 && _staticMap.Map[x, y] <= 0)
-                        continue;
-                    if (_dynamicMap._map[i, j] == 0)
-                        continue;
-
-                    _colored[x, y] += (sbyte)(_map[x, y] / 2);
-                    _colored[x, y] = (sbyte)((_colored[x, y] + (sbyte)(100 / max_queue_length)) < _colored[x, y] ? _colored[x, y] : (_colored[x, y] + (sbyte)(100 / max_queue_length)));
-                    if (_colored[x, y] >= 100)
-                    {
-                        _map[x, y] = 0;
-                        _obstacles[x, y, 0] = x;
-                        _obstacles[x, y, 1] = y;
-                        _colored[x, y] = 100;
-                        openList.Enqueue(new(x, y));
-                    }
-                }
-        }
-        while (openList.Count > 0)
-        {
-            var c = openList.Dequeue();
-            for (int i = -1; i <= 1; i++)
-                for (int j = -1; j <= 1; j++)
-                {
-                    var ci = c.x + i;
-                    var cj = c.y + j;
-                    if (i == 0 && j == 0)
-                        continue;
-                    if (ci < 0 || ci >= SizeX)
-                        continue;
-                    if (cj < 0 || cj >= SizeY)
-                        continue;
-
-                    // for calculate
-                    var cObstacleX = _obstacles[ci, cj, 0];
-                    var cObstacleY = _obstacles[ci, cj, 1];
-                    if (cObstacleX != -1)
-                    {
-                        var m = (sbyte)(Math.Clamp(Math.Round(Math.Sqrt(Math.Pow(cObstacleX - c.x, 2) + Math.Pow(cObstacleY - c.y, 2))) * Resolution / maxDistance, 0, 1) * 100);
-                        if (_map[c.x, c.y] > m)
-                        {
-                            _map[c.x, c.y] = m;
-                            _obstacles[c.x, c.y, 0] = cObstacleX;
-                            _obstacles[c.x, c.y, 1] = cObstacleY;
-                        }
-
-                    }
-                    // for enqueue
-                    if (_colored[ci, cj] == 100)
-                        continue;
-                    if (_obstacles[ci, cj, 0] == ci && _obstacles[ci, cj, 0] == cj)
-                        continue;
-                    _colored[ci, cj] = 100;
-                    openList.Enqueue((ci, cj));
-                }
-        }
+        ESDFBuilder.Build(_staticMap, SizeX, SizeY, _dynamicMaps, max_queue_length, maxDistance, ref _map, ref _obstacles);
         if (debug)
         {
             _esdfPublisher.Publish((_map, Resolution, (uint)SizeX, (uint)SizeY));
