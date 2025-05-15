b4eb6f5694388d7f1a1b844aae0490c50e3a7886
Alray <1780284652@qq.com>
2025-05-15 16:05:13 +0800

    在目标点附近0.1m内不做位移

--- src/TrajectoryTracer/Tracer.cs
+++ src/TrajectoryTracer/Tracer.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel;
+using System.Numerics;
+using ALPlanner.Interfaces;
 using ALPlanner.Interfaces.ROS;
 using TlarcKernel.IO.ProcessCommunicateInterfaces;
 
@@ -9,6 +11,10 @@ class Tracer : Component
   IO.ROS2Msgs.Geometry.Pose2D pose2D;
 
   public Vector3d velocity;
+
+  [ComponentReferenceFiled]
+  IPositionDecider target;
+
 
   [ComponentReferenceFiled]
   IPositionVelocityController controller;
@@ -24,9 +30,10 @@ class Tracer : Component
   {
     velocity = controller.ControlVolume(sentry.Position);
     velocity = Quaterniond.AxisAngleR(Vector3d.AxisZ, -sentry.AngleR) * velocity;
-    if (velocity.Length > 0.05)
-      pose2D.Publish((new((float)velocity.x, (float)velocity.y), 0));
+
+    if ((sentry.Position - target.TargetPosition).Length < 0.1)
+      pose2D.Publish(new());
     else
-      pose2D.Publish((new()));
+      pose2D.Publish((new Vector2((float)velocity.x, (float)velocity.y), 0));
   }
 }
