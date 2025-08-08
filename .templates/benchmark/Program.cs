00424a51453476f3d781be4a701bc1544b13e640
alray <1780284652@qq.com>
2025-08-08 15:54:57 +0800

    添加Runable和BenchMark模板

---
+++ .templates/benchmark/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+
+namespace Kernel.Benchmarks;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        // var sum = BenchmarkRunner.Run<TfCastBenchmarks>();
+        return;
+    }
+}
